5d8f41536c4aaf62aaecb0b8722bbe5fc6944067
Leonardo Gómez <39743862+lgomez9@users.noreply.github.com>
2020-01-14 22:58:21 -0600

    Update problem1295.java

--- Leetcode/problem1295.java
+++ Leetcode/problem1295.java
@@ -1,1 +1,24 @@
-
+class Solution {
+//     Function that returns whether the given number has an even number of digits
+    boolean evenDigits(int n) {
+        int numDigits = 0;
+        
+        while(n > 0) {
+            n /= 10;
+            numDigits++;
+        }
+        
+        return numDigits % 2 == 0;
+    }
+  
+//  Find the number of evens
+    public int findNumbers(int[] nums) {
+        int numEven = 0;
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(evenDigits(nums[i])) numEven++;
+        }
+        
+        return numEven;
+    }
+}
