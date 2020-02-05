e7643868593e9154d7f962d239356164fb88dcc6
Leonardo Gómez <39743862+lgomez9@users.noreply.github.com>
2020-02-04 20:56:37 -0600

    Create problem1237.java

---
+++ Leetcode/problem1237.java
@@ -0,0 +1,24 @@
+/*
+ * // This is the custom function interface.
+ * // You should not implement it, or speculate about its implementation
+ * class CustomFunction {
+ *     // Returns f(x, y) for any given positive integers x and y.
+ *     // Note that f(x, y) is increasing with respect to both x and y.
+ *     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ *     public int f(int x, int y);
+ * };
+ */
+class Solution {
+    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
+//         Create a list to return
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        
+        for(int x = 1; x <= 1000; x++) {
+            for(int y = 1; y <= 1000; y++) {
+                if(customfunction.f(x,y) == z) ret.add(new ArrayList<Integer>(Arrays.asList(x, y)));
+            }
+        }
+        
+        return ret;
+    }
+}
