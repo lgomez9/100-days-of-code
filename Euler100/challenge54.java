dea1fb1ad084d88920443ffdb45752e4182fefe4
Leonardo Gómez <39743862+lgomez9@users.noreply.github.com>
2020-04-01 16:00:08 -0500

    Add files via upload

--- Euler100/challenge54.java
+++ Euler100/challenge54.java
@@ -1,5 +1,6 @@
 // Challenge 54
 import java.util.*;
+import java.io.*;
 
 /* In the card game poker, a hand consists of five cards and are ranked, in order
  * from lowest to highest, in the following way:
@@ -114,8 +115,16 @@ class Hand {
     System.out.println();
   }
 
-  // Function that returns the highest value of a hand
-  int highestValue() { return cards[cards.length-1].value; }
+  // Function that returns the highest value of a n kind of a hand, less than t (top)
+  int highestValue(int n, int t) {
+    int highest = 0;
+
+    for(Integer value : values.keySet()) {
+      if(values.get(value) == n && value > highest && value < t) highest = value;
+    }
+
+    return highest;
+  }
 }
 
 public class challenge54 {
@@ -134,7 +143,7 @@ public class challenge54 {
   static boolean isStraightFlush(Hand h) {
     if(h.suits.size() > 1 || h.values.size() < 5) return false;
     for(int i = 1; i < h.cards.length; i++) {
-      if(h.cards[i].value <= h.cards[i-1].value) return false;
+      if(h.cards[i].value != h.cards[i-1].value+1) return false;
     }
 
     return true;
@@ -150,6 +159,10 @@ public class challenge54 {
 
     return false;
   }
+  static boolean compareFourOfAKind(Hand one, Hand two) {
+    if(one.highestValue(4, 15) != two.highestValue(4, 15)) return one.highestValue(4, 15) > two.highestValue(4, 15);
+    else return compareHighestValues(one, two);
+  }
 
   // Function to check if a hand is a full house
   static boolean isFullHouse(Hand h) {
@@ -157,6 +170,10 @@ public class challenge54 {
     if(h.values.size() > 2) return false;
 
     return true;
+  }
+  static boolean compareFullHouse(Hand one, Hand two) {
+    if(one.highestValue(3, 15) != two.highestValue(3, 15)) return one.highestValue(3, 15) > two.highestValue(3, 15);
+    else return compareOnePair(one, two);
   }
 
   // Function to check if a hand is a flush
@@ -168,7 +185,7 @@ public class challenge54 {
   static boolean isStraight(Hand h) {
     if(h.values.size() < 5) return false;
     for(int i = 1; i < h.cards.length; i++) {
-      if(h.cards[i].value <= h.cards[i-1].value) return false;
+      if(h.cards[i].value != h.cards[i-1].value+1) return false;
     }
 
     return true;
@@ -185,6 +202,10 @@ public class challenge54 {
 
     return false;
   }
+  static boolean compareThreeOfAKind(Hand one, Hand two) {
+    if(one.highestValue(3, 15) != two.highestValue(3, 15)) return one.highestValue(3, 15) > two.highestValue(3, 15);
+    else return compareHighestValues(one, two);
+  }
 
   // Function to check if a hand is two pairs
   static boolean isTwoPairs(Hand h) {
@@ -197,8 +218,16 @@ public class challenge54 {
 
     return pairCount == 2;
   }
+  static boolean compareTwoPairs(Hand one, Hand two) {
+    if(one.highestValue(2, 15) != two.highestValue(2, 15)) return one.highestValue(2, 15) > two.highestValue(2, 15);
+    else {
+      int top = one.highestValue(2, 15);
+      if(one.highestValue(2, top) != two.highestValue(2, top)) return one.highestValue(2, top) > two.highestValue(2, top);
+      else return compareHighestValues(one, two);
+    }
+  }
 
-  // Function to check if a hand is one pair
+  // Function to check if a hand is one pair, and function to compare one pairs
   static boolean isOnePair(Hand h) {
     if(h.values.size() != 4) return false;
     int pairCount = 0;
@@ -209,54 +238,95 @@ public class challenge54 {
 
     return pairCount == 1;
   }
+  static boolean compareOnePair(Hand one, Hand two) {
+    if(one.highestValue(2, 15) != two.highestValue(2, 15)) return one.highestValue(2, 15) > two.highestValue(2, 15);
+    else return compareHighestValues(one, two);
+  }
+
+  // Function to compare the single highest values in two hands
+  static boolean compareHighestValues(Hand one, Hand two) {
+    int top = 15;
+
+    while(one.highestValue(1, top) == two.highestValue(1, top)) top = one.highestValue(1, top);
+    return one.highestValue(1, top) > two.highestValue(1, top);
+  }
+
+  // Function that returns the value of a hand (1 - 10)
+  static int handValue(Hand h) {
+    if(isRoyalFlush(h)) return 10;
+    else if(isStraightFlush(h)) return 9;
+    else if(isFourOfAKind(h)) return 8;
+    else if(isFullHouse(h)) return 7;
+    else if(isFlush(h)) return 6;
+    else if(isStraight(h)) return 5;
+    else if(isThreeOfAKind(h)) return 4;
+    else if(isTwoPairs(h)) return 3;
+    else if(isOnePair(h)) return 2;
+    else return 1;
+  }
+
+  // Function to check if player one's hand wins
+  static boolean playerOneWins(Hand one, Hand two) {
+    int oneValue = handValue(one);
+    int twoValue = handValue(two);
+    // Values are 10 [Royal Flush] -> 1 [Highest Card]
+
+    // Check if the first value is greater than the second value
+    if(oneValue > twoValue) return true;
+    else if(oneValue < twoValue) return false;
+    else {
+      if(oneValue == 1) return compareHighestValues(one, two);
+      else if(oneValue == 2) return compareOnePair(one, two);
+      else if(oneValue == 3) return compareTwoPairs(one, two);
+      else if(oneValue == 4) return compareThreeOfAKind(one, two);
+      else if(oneValue == 5) return compareHighestValues(one, two);
+      else if(oneValue == 6) return compareHighestValues(one, two);
+      else if(oneValue == 7) return compareFullHouse(one, two);
+      else if(oneValue == 8) return compareFourOfAKind(one, two);
+      else if(oneValue == 9) return compareHighestValues(one, two);
+      else return true;
+    }
+  }
+
+  // Function that finds the winner of the given two hands
+  // True is first player wins, false is second player wins
+  static boolean winningHand(String hands) {
+    Hand handOne = new Hand(hands.substring(0, hands.length()/2));
+    Hand handTwo = new Hand(hands.substring(hands.length()/2 + 1));
+
+    return playerOneWins(handOne, handTwo);
+  }
 
   public static void main(String args[]) {
-    // Examples to test checking what kind of hand
-    // Hand royalFlush = new Hand("TD JD QD KD AD");
-    // royalFlush.printHand("Royal flush: ");
-    // System.out.println("This is a royal flush: " + isRoyalFlush(royalFlush));
-    //
-    // Hand straightFlush = new Hand("2D 4D 5D 3D 6D");
-    // straightFlush.printHand("Straight flush: ");
-    // System.out.println("This is a straight flush: " + isStraightFlush(straightFlush));
-
-    // Hand fourOfAKind = new Hand("2D 2S 2C 2H 3S");
-    // fourOfAKind.printHand("Four of a kind: ");
-    // System.out.println("This is a four of a kind: " + isFourOfAKind(fourOfAKind));
-
-    // Hand fullHouse = new Hand("2D 2S 2C 3H 3C");
-    // fullHouse.printHand("Full house: ");
-    // System.out.println("This is a full house: " + isFullHouse(fullHouse));
-
-    // Hand flush = new Hand("2D 3D 4D 5D 7D");
-    // flush.printHand("Flush: ");
-    // System.out.println("This is a flush: " + isFlush(flush));
-
-    // Hand straight = new Hand("7C 3D 4H 5C 6S");
-    // straight.printHand("Straight: ");
-    // System.out.println("This is a straight: " + isStraight(straight));
-
-    // Hand threeOfAKind = new Hand("2D 2C 2S 3D 5C");
-    // threeOfAKind.printHand("Three of a kind: ");
-    // System.out.println("This is a three of a kind: " + isThreeOfAKind(threeOfAKind));
-
-    // Hand twoPairs = new Hand("2D 2C 3D 3C 4H");
-    // twoPairs.printHand("Two pairs: ");
-    // System.out.println("This is two pairs: " + isTwoPairs(twoPairs));
-
-    // Hand onePair = new Hand("2D 2C 3D 7H 9D");
-    // onePair.printHand("One pair: ");
-    // System.out.println("This is one pair: " + isOnePair(onePair));
-
     // Examples for testing comparing two hands
-    String exHands1 = "5H 5C 6S 7S KD 2C 3S 8S 8D TD"; // Player 2
-    String exHands2 = "5D 8C 9S JS AC 2C 5C 7D 8S QH"; // Player 1
-    String exHands3 = "2D 9C AS AH AC 3D 6D 7D TD QD"; // Player 2
-    String exHands4 = "4D 6S 9H QH QC 3D 6D 7H QD QS"; // Player 1
-    String exHands5 = "2H 2D 4C 4D 4S 3C 3D 3S 9S 9D"; // Player 1
+    String ex1 = "5H 5C 6S 7S KD 2C 3S 8S 8D TD"; // Player 2
+    String ex2 = "5D 8C 9S JS AC 2C 5C 7D 8S QH"; // Player 1
+    String ex3 = "2D 9C AS AH AC 3D 6D 7D TD QD"; // Player 2
+    String ex4 = "4D 6S 9H QH QC 3D 6D 7H QD QS"; // Player 1
+    String ex5 = "2H 2D 4C 4D 4S 3C 3D 3S 9S 9D"; // Player 1
 
+    String[] examples = {ex1, ex2, ex3, ex4, ex5};
+    System.out.println();
+    // Checking the examples given
+    for(int i = 0; i < examples.length; i++) {
+      if(winningHand(examples[i])) System.out.println("For hand " + (i+1) + ", player 1 wins!\n");
+      else System.out.println("For hand " + (i+1) + ", player 2 wins!\n");
+    }
 
+    try {
+      // Create a reader to read the contents of the test file
+      BufferedReader br = new BufferedReader(new FileReader(new File("p054_poker.txt")));
 
+      String st;
+      int totalWins = 0;
 
+      while((st = br.readLine()) != null) {
+        if(winningHand(st)) totalWins++;
+      }
+
+      System.out.println("Player one wins " + totalWins + " games in total.");
+    } catch(Exception e) {
+      System.out.println(e);
+    }
   }
 }
